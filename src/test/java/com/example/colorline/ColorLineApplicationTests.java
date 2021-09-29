f7e0dc5e41c3497904df37ea7a3496b0eba56f12
RamiM <s351882@oslomet.no>
2021-09-29 19:16:03 +0200

    Initial commit

---
+++ src/test/java/com/example/colorline/ColorLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.colorline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
